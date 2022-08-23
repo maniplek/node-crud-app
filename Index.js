580e2cf7789bc0b1a2d6183fe5656e69b4c4b2d4
maniplek <maniplek81@gmail.com>
2022-08-23 23:57:54 +0200

    added more route

--- Index.js
+++ Index.js
@@ -2,21 +2,42 @@ const { request } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'},
+]
+
 app.get('/', (req,res)=>{
     res.send('hello world')
 })
 
 app.get('/api/courses', (req,res)=>{
-    res.send([1,2,3,4,5])
+    res.send(courses);
 });
 
+app.post('/api/courses', (req,res)=>{
+    const course = {
+        id: courses.id,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+} );
+
+
+
 app.get('/api/courses/:id', (req,res)=>{
-    res.send(req.params);
+    const course = courses.find(c => c.id === parseInt(req.params.id) );
+    if(!course) res.status(404).send('course id requested not found');
+    res.status(200).send(course);
 })
-//for reading query parameter
-app.get('/api/post/:year/:month', (req,res)=>{
-    res.send(req.query);
-})
+// //for reading query parameter
+// app.get('/api/post/:year/:month', (req,res)=>{
+//     res.send(req.query);
+// })
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`running on port ${port}`));
